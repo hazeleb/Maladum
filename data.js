1cc6850909910022abf9085048bed44a9f5b5e4f
hazeleb <112207276+hazeleb@users.noreply.github.com>
2025-04-20 07:12:51 -0400

    Add files via upload

--- data.js
+++ data.js
@@ -6,15 +6,20 @@ const myData = {
                     <area id="Unarmed Combat"  coords="334,125,384,169" shape="rect" alt="Unarmed Combat" title="Unarmed Combat" href="javascript:;">
                     <area id="Size"  coords="346,191,384,235" shape="rect" alt="Size" title="Size" href="javascript:;">
                 </map>`,
-        "mobile_map": `<map name="Grogmar" id="Grogmar_map">
-                    <area id="Quick Recovery"  coords="243,146,266,162" shape="rect" alt="Quick Recovery" title="Quick Recovery" href="javascript:;">
-                    <area id="Unarmed Combat"  coords="167,62,192,84" shape="rect" alt="Unarmed Combat" title="Unarmed Combat" href="javascript:;">
-                    <area id="Size"  coords="173,95,192,117" shape="rect" alt="Size" title="Size" href="javascript:;">
+        "mobile_map": 
+                `<map name="Grogmar" id="Grogmar_map">
+                    <area id="Quick Recovery"  coords="242,147,268,165" shape="rect" alt="Quick Recovery" title="Quick Recovery" href="javascript:;">
+                    <area id="Unarmed Combat"  coords="170,63,192,84" shape="rect" alt="Unarmed Combat" title="Unarmed Combat" href="javascript:;">
+                    <area id="Size"  coords="169,92,192,117" shape="rect" alt="Size" title="Size" href="javascript:;">
                 </map>`
     },
     "Artain": {   
         "map": `<map name="Artain" id="Artain_map">
                     <area id="Entertainer"  coords="347,165,400,212" shape="rect" alt="Entertainer" title="Entertainer" href="javascript:;">
+                </map>`,
+        "mobile_map": 
+                `<map name="Artain" id="Artain_map">
+                    <area id="Entertainer"  coords="175,86,198,100" shape="rect" alt="Entertainer" title="Entertainer" href="javascript:;">
                 </map>`
     },
     "Moranna": {   
@@ -22,6 +27,12 @@ const myData = {
                     <area id="Impervious"  coords="513,68,564,117" shape="rect" alt="Impervious" title="Impervious" href="javascript:;">
                     <area id="Malacyte Mastery Spells"  coords="518,170,576,214" shape="rect" alt="Malacyte Mastery" title="Malacyte Mastery" href="javascript:;">
                     <area id="Acrobatics"  coords="485,285,544,334" shape="rect" alt="Acrobatics" title="Acrobatics" href="javascript:;">
+                </map>`,
+        "mobile_map": 
+                `<map name="Moranna" id="Moranna_map">
+                    <area id="Impervious"  coords="260,37,280,55" shape="rect" alt="Impervious" title="Impervious" href="javascript:;">
+                    <area id="Malacyte Mastery Spells"  coords="260,89,284,105" shape="rect" alt="Malacyte Mastery" title="Malacyte Mastery" href="javascript:;">
+                    <area id="Acrobatics"  coords="244,146,269,165" shape="rect" alt="Acrobatics" title="Acrobatics" href="javascript:;">
                 </map>`
     },
     "Nerinda": {   
@@ -29,18 +40,34 @@ const myData = {
                     <area id="Unarmed Combat"  coords="507,95,551,134" shape="rect" alt="Unarmed Combat" title="Unarmed Combat" href="javascript:;">
                     <area id="Weapons Master"  coords="507,172,565,218" shape="rect" alt="Weapons Master" title="Weapons Master" href="javascript:;">
                     <area id="Fleet of Foot"  coords="485,280,546,332" shape="rect" alt="Fleet of Foot" title="Fleet of Foot" href="javascript:;">
+                </map>`,
+        "mobile_map": 
+                `<map name="Nerinda" id="Nerinda_map">
+                    <area id="Unarmed Combat"  coords="253,46,275,66" shape="rect" alt="Unarmed Combat" title="Unarmed Combat" href="javascript:;">
+                    <area id="Weapons Master"  coords="257,88,280,105" shape="rect" alt="Weapons Master" title="Weapons Master" href="javascript:;">
+                    <area id="Fleet of Foot"  coords="246,147,269,161" shape="rect" alt="Fleet of Foot" title="Fleet of Foot" href="javascript:;">
                 </map>`
     },
     "Syrio": {   
         "map": `<map name="Syrio" id="Syrio_map">
                     <area id="Reflexes"  coords="347,177,398,218" shape="rect" alt="Reflexes" title="Reflexes" href="javascript:;">
                     <area id="Fast"  coords="488,289,515,344" shape="rect" alt="Fast" title="Fast" href="javascript:;">
+                </map>`,
+        "mobile_map": 
+                `<map name="Syrio" id="Syrio_map">
+                    <area id="Reflexes"  coords="174,91,197,105" shape="rect" alt="Reflexes" title="Reflexes" href="javascript:;">
+                    <area id="Fast"  coords="244,143,259,171" shape="rect" alt="Fast" title="Fast" href="javascript:;">
                 </map>`
     },
     "Unger": {   
         "map": `<map name="Unger" id="Unger_map">
                     <area id="Persuasion"  coords="346,173,399,212" shape="rect" alt="Persuasion" title="Persuasion" href="javascript:;">
                     <area id="Detect Spell"  coords="488,289,549,327" shape="rect" alt="Detect Spell" title="Detect Spell" href="javascript:;">
+                </map>`,
+        "mobile_map": 
+                `<map name="Unger" id="Unger_map">
+                    <area id="Persuasion"  coords="174,89,196,104" shape="rect" alt="Persuasion" title="Persuasion" href="javascript:;">
+                    <area id="Detect Spell"  coords="246,144,274,157" shape="rect" alt="Detect Spell" title="Detect Spell" href="javascript:;">
                 </map>`
     },
     // trades
@@ -59,9 +86,20 @@ const myData = {
                     <area id="Less Health"  coords="534,82,578,130" shape="rect" alt="Less Health" title="Less Health" href="javascript:;">
                     <area id="Extra Skill"  coords="528,152,568,177" shape="rect" alt="Extra Skill" title="Extra Skill" href="javascript:;">
                 </map>`,
-        "mobile_map": `<map name="Curator" id="Curator_map">
-                    <area id="Loremaster"  coords="14,55,36,75" shape="rect" alt="Loremaster" title="Loremaster" href="javascript:;">
-                    <area id="Herbalism"  coords="45,37,65,55" shape="rect" alt="Herbalism" title="Herbalism" href="javascript:;">
+        "mobile_map": 
+                `<map name="Curator" id="Curator_map">
+                    <area id="Loremaster"  coords="16,57,36,73" shape="rect" alt="Loremaster" title="Loremaster" href="javascript:;">
+                    <area id="Herbalism"  coords="45,38,63,57" shape="rect" alt="Herbalism" title="Herbalism" href="javascript:;">
+                    <area id="Unlikely Hero"  coords="76,57,97,74" shape="rect" alt="Unlikely Hero" title="Unlikely Hero" href="javascript:;">
+                    <area id="Smithing"  coords="98,23,117,40" shape="rect" alt="Smithing" title="Smithing" href="javascript:;">
+                    <area id="Duck for Cover"  coords="123,55,145,73" shape="rect" alt="Duck for Cover" title="Duck for Cover" href="javascript:;">
+                    <area id="Tactical Gift"  coords="146,20,166,42" shape="rect" alt="Tactical Gift" title="Tactical Gift" href="javascript:;">
+                    <area id="Barter"  coords="171,56,193,73" shape="rect" alt="Barter" title="Barter" href="javascript:;">
+                    <area id="Tracking"  coords="192,22,215,41" shape="rect" alt="Tracking" title="Tracking" href="javascript:;">
+                    <area id="Natural Remedies"  coords="218,55,240,73" shape="rect" alt="Natural Remedies" title="Natural Remedies" href="javascript:;">
+                    <area id="Malacyte Mastery Spells"  coords="244,20,269,41" shape="rect" alt="Malacyte Mastery" title="Malacyte Mastery" href="javascript:;">
+                    <area id="Less Health"  coords="274,49,291,65" shape="rect" alt="Less Health" title="Less Health" href="javascript:;">
+                    <area id="Extra Skill"  coords="264,75,294,90" shape="rect" alt="Extra Skill" title="Extra Skill" href="javascript:;">
                 </map>`
     },
     "Marksman": {   
@@ -75,6 +113,18 @@ const myData = {
                     <area id="Fleet of Foot"  coords="402,85,454,154" shape="rect" alt="Fleet of Foot" title="Fleet of Foot" href="javascript:;">
                     <area id="Reflexes"  coords="461,42,509,87" shape="rect" alt="Reflexes" title="Reflexes" href="javascript:;">
                     <area id="Hawkeye"  coords="532,134,577,180" shape="rect" alt="Hawkeye" title="Hawkeye" href="javascript:;">        
+                </map>`,
+        "mobile_map": 
+                `<map name="Marksman" id="Marksman_map">
+                    <area id="Trick Shot"  coords="17,56,41,76" shape="rect" alt="Trick Shot" title="Trick Shot" href="javascript:;">
+                    <area id="Ranged Expert"  coords="50,37,73,55" shape="rect" alt="Ranged Expert" title="Ranged Expert" href="javascript:;">
+                    <area id="Counter Shot"  coords="87,56,110,72" shape="rect" alt="Counter Shot" title="Counter Shot" href="javascript:;">
+                    <area id="Bullseye"  coords="117,20,141,40" shape="rect" alt="Bullseye" title="Bullseye" href="javascript:;">
+                    <area id="Ambush"  coords="146,58,169,75" shape="rect" alt="Ambush" title="Ambush" href="javascript:;">
+                    <area id="Duck for Cover"  coords="171,23,197,40" shape="rect" alt="Duck for Cover" title="Duck for Cover" href="javascript:;">
+                    <area id="Fleet of Foot"  coords="201,56,226,76" shape="rect" alt="Fleet of Foot" title="Fleet of Foot" href="javascript:;">
+                    <area id="Reflexes"  coords="230,24,254,44" shape="rect" alt="Reflexes" title="Reflexes" href="javascript:;">
+                    <area id="Hawkeye"  coords="272,66,289,86" shape="rect" alt="Hawkeye" title="Hawkeye" href="javascript:;">        
                 </map>`
     },
     "Paladin": {   
@@ -87,6 +137,17 @@ const myData = {
                     <area id="Weapons Master"  coords="402,108,461,150" shape="rect" alt="Weapons Master" title="Weapons Master" href="javascript:;">
                     <area id="Disarm"  coords="462,33,517,75" shape="rect" alt="Disarm" title="Disarm" href="javascript:;">
                     <area id="Persuasion"  coords="520,115,575,149" shape="rect" alt="Persuasion" title="Persuasion" href="javascript:;">
+                </map>`,
+        "mobile_map": 
+                `<map name="Paladin" id="Paladin_map">
+                    <area id="Paladin Spells"  coords="5,32,59,90" shape="rect" alt="Paladin Spells" title="Paladin Spells" href="javascript:;">
+                    <area id="Inspiring"  coords="81,56,107,77" shape="rect" alt="Inspiring" title="Inspiring" href="javascript:;">
+                    <area id="Barter"  coords="113,23,137,41" shape="rect" alt="Barter" title="Barter" href="javascript:;">
+                    <area id="Steady"  coords="144,55,168,77" shape="rect" alt="Steady" title="Steady" href="javascript:;">
+                    <area id="Impervious"  coords="171,21,197,40" shape="rect" alt="Impervious" title="Impervious" href="javascript:;">
+                    <area id="Weapons Master"  coords="203,54,229,75" shape="rect" alt="Weapons Master" title="Weapons Master" href="javascript:;">
+                    <area id="Disarm"  coords="232,19,517,38" shape="rect" alt="Disarm" title="Disarm" href="javascript:;">
+                    <area id="Persuasion"  coords="261,53,286,74" shape="rect" alt="Persuasion" title="Persuasion" href="javascript:;">
                 </map>`
     },
     "Rambler": {   
@@ -101,6 +162,19 @@ const myData = {
                     <area id="Immunity to Entangling"  coords="540,51,583,110" shape="rect" alt="Immunity to Entangling" title="Immunity to Entangling" href="javascript:;">
                     <area id="Recover Spell"  coords="441,124,500,163" shape="rect" alt="Recover Spell" title="Recover Spell" href="javascript:;">
                     <area id="Detect Spell"  coords="525,125,588,159" shape="rect" alt="Detect Spell" title="Detect Spell" href="javascript:;">
+                </map>`,
+        "mobile_map": 
+                `<map name="Rambler" id="Rambler_map">
+                    <area id="Herbalism"  coords="21,41,37,60" shape="rect" alt="Herbalism" title="Herbalism" href="javascript:;">
+                    <area id="Tracking"  coords="50,55,74,75" shape="rect" alt="Tracking" title="Tracking" href="javascript:;">
+                    <area id="Fleet of Foot"  coords="85,23,109,39" shape="rect" alt="Fleet of Foot" title="Fleet of Foot" href="javascript:;">
+                    <area id="Quick Recovery"  coords="114,57,142,75" shape="rect" alt="Quick Recovery" title="Quick Recovery" href="javascript:;">
+                    <area id="Tricks of the Trade"  coords="143,19,168,39" shape="rect" alt="Tricks of the Trade" title="Tricks of the Trade" href="javascript:;">
+                    <area id="Hard to Hit"  coords="172,55,199,73" shape="rect" alt="Hard to Hit" title="Hard to Hit" href="javascript:;">
+                    <area id="One with Nature"  coords="202,19,227,40" shape="rect" alt="One with Nature" title="One with Nature" href="javascript:;">
+                    <area id="Immunity to Entangling"  coords="271,26,292,50" shape="rect" alt="Immunity to Entangling" title="Immunity to Entangling" href="javascript:;">
+                    <area id="Recover Spell"  coords="222,63,248,78" shape="rect" alt="Recover Spell" title="Recover Spell" href="javascript:;">
+                    <area id="Detect Spell"  coords="265,65,295,79" shape="rect" alt="Detect Spell" title="Detect Spell" href="javascript:;">
                 </map>`
     },
     "Rook": {   
@@ -112,6 +186,16 @@ const myData = {
                     <area id="Persuasion"  coords="402,109,457,149" shape="rect" alt="Persuasion" title="Persuasion" href="javascript:;">
                     <area id="Barter"  coords="460,36,514,79" shape="rect" alt="Barter" title="Barter" href="javascript:;">
                     <area id="Duck for Cover"  coords="518,111,576,147" shape="rect" alt="Duck for Cover" title="Duck for Cover" href="javascript:;">    
+                </map>`,
+        "mobile_map": 
+                `<map name="Rook" id="Rook_map">
+                    <area id="Rook Spells"  coords="10,26,81,93" shape="rect" alt="Rook Spells" title="Rook Spells" href="javascript:;">
+                    <area id="Power Manipulation"  coords="110,23,135,40" shape="rect" alt="Power Manipulation" title="Power Manipulation" href="javascript:;">
+                    <area id="Malacyte Mastery Spells"  coords="142,56,166,75" shape="rect" alt="Malacyte Mastery" title="Malacyte Mastery" href="javascript:;">
+                    <area id="Distraction"  coords="172,20,195,40" shape="rect" alt="Distraction" title="Distraction" href="javascript:;">
+                    <area id="Persuasion"  coords="200,56,226,74" shape="rect" alt="Persuasion" title="Persuasion" href="javascript:;">
+                    <area id="Barter"  coords="232,17,255,41" shape="rect" alt="Barter" title="Barter" href="javascript:;">
+                    <area id="Duck for Cover"  coords="259,56,286,75" shape="rect" alt="Duck for Cover" title="Duck for Cover" href="javascript:;">    
                 </map>`
     },
     "Sellsword": {   
@@ -125,6 +209,18 @@ const myData = {
                     <area id="Quick Recovery"  coords="388,112,442,145" shape="rect" alt="Quick Recovery" title="Quick Recovery" href="javascript:;">
                     <area id="Ambush"  coords="450,38,496,81" shape="rect" alt="Ambush" title="Ambush" href="javascript:;">
                     <area id="Extra Skill"  coords="545,141,578,169" shape="rect" alt="Extra Skill" title="Extra Skill" href="javascript:;">        
+                </map>`,
+        "mobile_map": 
+                `<map name="Sellsword" id="Sellsword_map">
+                    <area id="Reflexes"  coords="15,60,36,78" shape="rect" alt="Reflexes" title="Reflexes" href="javascript:;">
+                    <area id="Frenzy"  coords="46,40,71,61" shape="rect" alt="Frenzy" title="Frenzy" href="javascript:;">
+                    <area id="Weapons Master"  coords="80,58,106,76" shape="rect" alt="Weapons Master" title="Weapons Master" href="javascript:;">
+                    <area id="Counter Shot"  coords="108,23,134,43" shape="rect" alt="Counter Shot" title="Counter Shot" href="javascript:;">
+                    <area id="Bullseye"  coords="142,56,164,74" shape="rect" alt="Bullseye" title="Bullseye" href="javascript:;">
+                    <area id="Training"  coords="168,22,189,42" shape="rect" alt="Training" title="Training" href="javascript:;">
+                    <area id="Quick Recovery"  coords="197,54,221,72" shape="rect" alt="Quick Recovery" title="Quick Recovery" href="javascript:;">
+                    <area id="Ambush"  coords="225,20,248,41" shape="rect" alt="Ambush" title="Ambush" href="javascript:;">
+                    <area id="Extra Skill"  coords="275,74,289,85" shape="rect" alt="Extra Skill" title="Extra Skill" href="javascript:;">        
                 </map>`
     }   
 };
